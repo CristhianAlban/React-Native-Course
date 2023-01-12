2ec0bc75557cf79905431ac3877df6400100121c
cristhian alban <djstylecali@gmail.com>
2023-01-12 16:05:30 -0500

    feat: 09 backend node and mongoDB -- feat: 10  login producs app seccion 19 finish

---
+++ 10-ProductosApp/src/api/cafeApi.tsx
@@ -0,0 +1,20 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const baseURL = 'http://192.168.10.149:8080/api';
+
+const cafeApi = axios.create({ baseURL });
+
+cafeApi.interceptors.request.use(
+    async(config:AxiosRequestConfig<any>) => {
+        const token = await AsyncStorage.getItem('token');
+        if ( token ) {
+            config.headers['x-token'] = token;
+        }
+        return config;
+    }
+);
+
+
+
+export default cafeApi;
