3a84dd14f910c74f5d1415391d2b8640a6bbc56a
cristhian alban <djstylecali@gmail.com>
2023-01-12 16:24:56 -0500

    feat: products page and product add and update. section 20 finish

--- 10-ProductosApp/src/navigator/Navigator.tsx
+++ 10-ProductosApp/src/navigator/Navigator.tsx
@@ -8,6 +8,8 @@ import { RegisterScreen } from '../screens/RegisterScreen';
 import { ProtectedScreen } from '../screens/ProtectedScreen';
 import { LoadingScreen } from '../screens/LoadingScreen';
 
+import { ProductsNavigator } from './ProductsNavigator';
+
 
 const Stack = createStackNavigator();
 
@@ -36,7 +38,10 @@ export const Navigator = () => {
             </>
           )
           : (
-            <Stack.Screen name="ProtectedScreen" component={ ProtectedScreen } />
+            <>
+              <Stack.Screen name="ProductsNavigator" component={ ProductsNavigator } />
+              <Stack.Screen name="ProtectedScreen" component={ ProtectedScreen } />
+            </>
           )
       }
 
